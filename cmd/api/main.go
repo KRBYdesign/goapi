a43edb0dbb46c57a8e92025e098df0531b6bc5fb
Kyle Beasley <kyle.beasley17@gmail.com>
2024-09-12 13:23:50 -0500

    new api handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,3 +8,27 @@ import (
 	"github.com/KRBYdesign/goapi/internal/handlers"
 	log "github.com/sirupsen/logrus"
 )
+
+func main() {
+	log.SetReportCaller(true)
+	var router *chi.Mux = chi.NewRouter()
+	handlers.Handler(router)
+
+	fmt.Println("Starting GO API service...")
+	fmt.Println(`
+  /$$$$$$   /$$$$$$         /$$$$$$  /$$$$$$$  /$$$$$$
+ /$$__  $$ /$$__  $$       /$$__  $$| $$__  $$|_  $$_/
+| $$  \__/| $$  \ $$      | $$  \ $$| $$  \ $$  | $$  
+| $$ /$$$$| $$  | $$      | $$$$$$$$| $$$$$$$/  | $$  
+| $$|_  $$| $$  | $$      | $$__  $$| $$____/   | $$  
+| $$  \ $$| $$  | $$      | $$  | $$| $$        | $$  
+|  $$$$$$/|  $$$$$$/      | $$  | $$| $$       /$$$$$$
+ \______/  \______/       |__/  |__/|__/      |______/
+`)
+
+	err := http.ListenAndServe("localhost:8000", router)
+
+	if err != nil {
+		log.Error(err)
+	}
+}
